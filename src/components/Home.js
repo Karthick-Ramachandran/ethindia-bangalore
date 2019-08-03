e46a33753765d49eabf82eb9cbfc59383ba848a5
Karthick <karthiram165@gmail.com>
2019-08-03 14:22:59 +0530

    eth india

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar';
+
+export default () => {
+  return (
+    <div>
+        <Navbar/>
+      Hello vjvjhch
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../navbar.css';
+
+export default () => {
+  return (
+    <div>
+       <nav>
+ 
+    <div className="footer">
+        tcdtg
+    </div>
+    <div>
+        wedklwc
+    </div>
+    <div>
+        wcnkwdnc
+    </div>
+    </nav>
+    </div>
+  )
+}
